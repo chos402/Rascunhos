c36f9424a6f6db156793acdfae64f865b4152332
Carlos Henrique Oliveira Souza <110339916+chos402@users.noreply.github.com>
2025-06-16 14:04:31 -0300

    Create cad_contato.js

---
+++ atividade9/cad_contato.js
@@ -0,0 +1,19 @@
+function incluirContato() {
+  alert("Contato incluído!");
+}
+
+function editarContato() {
+  alert("Editar contato.");
+}
+
+function salvarContato() {
+  alert("Contato salvo!");
+}
+
+function cancelarEdicao() {
+  alert("Edição cancelada.");
+}
+
+function excluirContato() {
+  alert("Contato excluído!");
+}
